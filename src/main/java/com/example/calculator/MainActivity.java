35e4e8c8ea4e2d5e61f60e8e28d096a8ee8d8235
ivanfg9 <56409211+ivanfg9@users.noreply.github.com>
2021-03-18 22:49:32 +0100

    Add files via upload

--- src/main/java/com/example/calculator/MainActivity.java
+++ src/main/java/com/example/calculator/MainActivity.java
@@ -4,30 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity{
 
-    protected Calculator calculator;
+    protected Calculator calculator = new Calculator();
     protected EditText editTextX;
     protected EditText editTextY;
-    public static final String EXTRA_ADD_RESULT = "resultado";
+
+    private static final String LOG = MainActivity.class.getSimpleName();
+    static final String EXTRA_ADD_RESULT = "resultado";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.editTextNumberDecimal);
-        findViewById(R.id.editTextNumberDecimal2);
+        editTextX = findViewById(R.id.editTextNumberDecimal);
+        editTextY = findViewById(R.id.editTextNumberDecimal2);
+        Log.d(LOG,"UI creada correctamente");
     }
 
-    protected void addXandY(View v){
-        //
+    public void addXandY(View v){
         Integer elem1 = Integer.parseInt(editTextX.toString().trim());
         Integer elem2 = Integer.parseInt(editTextY.toString().trim());
         Integer result = calculator.add(elem1,elem2);
-        //crear objeto intent
+
+        Log.d(LOG,"Resultado con x (" + elem1 + ") y con y (" + elem2 + "): " + result);
+
         Intent showResultIntent = new Intent(this, CalculatorResultActivity.class);
         showResultIntent.putExtra(EXTRA_ADD_RESULT, result);
         startActivity(showResultIntent);
